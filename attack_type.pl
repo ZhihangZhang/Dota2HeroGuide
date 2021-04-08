8bddf9194d0b7939261007d10d72ed72febcd181
ZhihangZhang <462143015zzh@gmail.com>
2021-04-08 12:04:08 -0700

    split into multiple files

---
+++ attack_type.pl
@@ -0,0 +1,123 @@
+% Melee
+prop(abaddon, attack_type, melee).
+prop(alchemist, attack_type, melee).
+prop(anti-mage, attack_type, melee).
+prop(axe, attack_type, melee).
+prop(beastmaster, attack_type, melee).
+prop(bloodseeker, attack_type, melee).
+prop(bounty_hunter, attack_type, melee).
+prop(brewmaster, attack_type, melee).
+prop(bristleback, attack_type, melee).
+prop(broodmother, attack_type, melee).
+prop(centaur_warrunner, attack_type, melee).
+prop(chaos_knight, attack_type, melee).
+prop(clockwerk, attack_type, melee).
+prop(dark_seer, attack_type, melee).
+prop(doom, attack_type, melee).
+prop(dragon_knight, attack_type, melee).
+prop(earth_spirit, attack_type, melee).
+prop(earthshaker, attack_type, melee).
+prop(elder_titan, attack_type, melee).
+prop(ember_spirit, attack_type, melee).
+prop(faceless_void, attack_type, melee).
+prop(juggernaut, attack_type, melee).
+prop(kunkka, attack_type, melee).
+prop(legion_commander, attack_type, melee).
+prop(lifestealer, attack_type, melee).
+prop(lycan, attack_type, melee).
+prop(magnus, attack_type, melee).
+prop(mars, attack_type, melee).
+prop(meepo, attack_type, melee).
+prop(monkey_king, attack_type, melee).
+prop(naga_siren, attack_type, melee).
+prop(night_stalker, attack_type, melee).
+prop(nyx_assassin, attack_type, melee).
+prop(ogre_magi, attack_type, melee).
+prop(omniknight, attack_type, melee).
+prop(pangolier, attack_type, melee).
+prop(phantom_assassin, attack_type, melee).
+prop(phantom_lancer, attack_type, melee).
+prop(pudge, attack_type, melee).
+prop(riki, attack_type, melee).
+prop(sand_king, attack_type, melee).
+prop(slardar, attack_type, melee).
+prop(slark, attack_type, melee).
+prop(spectre, attack_type, melee).
+prop(spirit_breaker, attack_type, melee).
+prop(sven, attack_type, melee).
+prop(terrorblade, attack_type, melee).
+prop(tidehunter, attack_type, melee).
+prop(timbersaw, attack_type, melee).
+prop(tiny, attack_type, melee).
+prop(treant_protector, attack_type, melee).
+prop(tusk, attack_type, melee).
+prop(underlord, attack_type, melee).
+prop(undying, attack_type, melee).
+prop(ursa, attack_type, melee).
+prop(void_spirit, attack_type, melee).
+prop(wraith_king, attack_type, melee).
+
+% Ranged
+prop(ancient_apparition , attack_type, ranged).
+prop(arc_warden , attack_type, ranged).
+prop(bane , attack_type, ranged).
+prop(batrider , attack_type, ranged).
+prop(chen , attack_type, ranged).
+prop(clinkz , attack_type, ranged).
+prop(crystal_maiden , attack_type, ranged).
+prop(dark_willow , attack_type, ranged).
+prop(dazzle , attack_type, ranged).
+prop(death_prophet , attack_type, ranged).
+prop(disruptor , attack_type, ranged).
+prop(drow_ranger , attack_type, ranged).
+prop(enchantress , attack_type, ranged).
+prop(enigma , attack_type, ranged).
+prop(grimstroke , attack_type, ranged).
+prop(gryocopter , attack_type, ranged).
+prop(hoodwink , attack_type, ranged).
+prop(huskar , attack_type, ranged).
+prop(invoker , attack_type, ranged).
+prop(io , attack_type, ranged).
+prop(jakiro , attack_type, ranged).
+prop(keeper_of_the_light , attack_type, ranged).
+prop(leshrac , attack_type, ranged).
+prop(lich , attack_type, ranged).
+prop(lina , attack_type, ranged).
+prop(lion , attack_type, ranged).
+prop(lone_druid , attack_type, ranged).
+prop(luna , attack_type, ranged).
+prop(medusa , attack_type, ranged).
+prop(mirana , attack_type, ranged).
+prop(morphling , attack_type, ranged).
+prop(natures_prophet , attack_type, ranged).
+prop(necrophos , attack_type, ranged).
+prop(oracle , attack_type, ranged).
+prop(outworld_destroyer , attack_type, ranged).
+prop(phoenix , attack_type, ranged).
+prop(puck , attack_type, ranged).
+prop(pugna , attack_type, ranged).
+prop(queen_of_pain , attack_type, ranged).
+prop(razor , attack_type, ranged).
+prop(rubick , attack_type, ranged).
+prop(shadow_demon , attack_type, ranged).
+prop(shadow_fiend , attack_type, ranged).
+prop(shadow_shaman , attack_type, ranged).
+prop(silencer , attack_type, ranged).
+prop(skywrath_mage , attack_type, ranged).
+prop(snapfire , attack_type, ranged).
+prop(sniper , attack_type, ranged).
+prop(storm_spirit , attack_type, ranged).
+prop(techies , attack_type, ranged).
+prop(templar_assassin , attack_type, ranged).
+prop(tinker , attack_type, ranged).
+prop(troll_warlord , attack_type, ranged).
+prop(vengeful_spirit , attack_type, ranged).
+prop(venomancer , attack_type, ranged).
+prop(viper , attack_type, ranged).
+prop(visage , attack_type, ranged).
+prop(warlock , attack_type, ranged).
+prop(weaver , attack_type, ranged).
+prop(windranger , attack_type, ranged).
+prop(winter_wyvern , attack_type, ranged).
+prop(witch_doctor , attack_type, ranged).
+prop(zeus, attack_type, ranged).
---
+++ attribute.pl
@@ -0,0 +1,125 @@
+% Strength
+prop(abaddon, attribute, strength).
+prop(alchemist, attribute, strength).
+prop(axe, attribute, strength).
+prop(beastmaster, attribute, strength).
+prop(brewmaster, attribute, strength).
+prop(bristleback, attribute, strength).
+prop(centaur_warrunner, attribute, strength).
+prop(chaos_knight, attribute, strength).
+prop(clockwerk, attribute, strength).
+prop(doom, attribute, strength).
+prop(dragon_knight, attribute, strength).
+prop(earth_spirit, attribute, strength).
+prop(earthshaker, attribute, strength).
+prop(elder_titan, attribute, strength).
+prop(huskar, attribute, strength).
+prop(io, attribute, strength).
+prop(kunkka, attribute, strength).
+prop(legion_commander, attribute, strength).
+prop(lifestealer, attribute, strength).
+prop(lycan, attribute, strength).
+prop(magnus, attribute, strength).
+prop(mars, attribute, strength).
+prop(night_stalker, attribute, strength).
+prop(omniknight, attribute, strength).
+prop(phoenix, attribute, strength).
+prop(pudge, attribute, strength).
+prop(sand_king, attribute, strength).
+prop(slardar, attribute, strength).
+prop(snapfire, attribute, strength).
+prop(spirit_breaker, attribute, strength).
+prop(sven, attribute, strength).
+prop(tidehunter, attribute, strength).
+prop(timbersaw, attribute, strength).
+prop(tiny, attribute, strength).
+prop(treant_protector, attribute, strength).
+prop(tusk, attribute, strength).
+prop(underlord, attribute, strength).
+prop(undying, attribute, strength).
+prop(wraith_king, attribute, strength).
+
+% Agility
+prop(anti-mage , attribute, agility).
+prop(arc_warden , attribute, agility).
+prop(bloodseeker , attribute, agility).
+prop(bounty_hunter , attribute, agility).
+prop(broodmother , attribute, agility).
+prop(clinkz , attribute, agility).
+prop(drow_ranger , attribute, agility).
+prop(ember_spirit , attribute, agility).
+prop(faceless_void , attribute, agility).
+prop(gyrocopter , attribute, agility).
+prop(hoodwink , attribute, agility).
+prop(juggernaut , attribute, agility).
+prop(lone_druid , attribute, agility).
+prop(luna , attribute, agility).
+prop(medusa , attribute, agility).
+prop(meepo , attribute, agility).
+prop(mirana , attribute, agility).
+prop(monkey_king , attribute, agility).
+prop(morphling , attribute, agility).
+prop(naga_siren , attribute, agility).
+prop(nyx_assassin , attribute, agility).
+prop(pangolier , attribute, agility).
+prop(phantom_assassin , attribute, agility).
+prop(phantom_lancer , attribute, agility).
+prop(razor , attribute, agility).
+prop(riki , attribute, agility).
+prop(shadow_fiend , attribute, agility).
+prop(slark , attribute, agility).
+prop(sniper , attribute, agility).
+prop(spectre , attribute, agility).
+prop(templar_assassin , attribute, agility).
+prop(terrorblade , attribute, agility).
+prop(troll_warlord , attribute, agility).
+prop(ursa , attribute, agility).
+prop(vengeful_spirit , attribute, agility).
+prop(venomancer , attribute, agility).
+prop(viper , attribute, agility).
+prop(weaver , attribute, agility).
+
+% Intelligence
+prop(ancient_apparition , attribute, intelligence).
+prop(bane , attribute, intelligence).
+prop(batrider , attribute, intelligence).
+prop(chen , attribute, intelligence).
+prop(crystal_maiden , attribute, intelligence).
+prop(dark_seer , attribute, intelligence).
+prop(dark_willow , attribute, intelligence).
+prop(dazzle , attribute, intelligence).
+prop(death_prophet , attribute, intelligence).
+prop(disruptor , attribute, intelligence).
+prop(enchantress , attribute, intelligence).
+prop(enigma , attribute, intelligence).
+prop(grimstroke , attribute, intelligence).
+prop(invoker , attribute, intelligence).
+prop(jakiro , attribute, intelligence).
+prop(keeper_of_the_light , attribute, intelligence).
+prop(leshrac , attribute, intelligence).
+prop(lich , attribute, intelligence).
+prop(lina , attribute, intelligence).
+prop(lion , attribute, intelligence).
+prop(natures_prophet , attribute, intelligence).
+prop(necrophos , attribute, intelligence).
+prop(ogre_magi , attribute, intelligence).
+prop(oracle , attribute, intelligence).
+prop(outworld_destroyer , attribute, intelligence).
+prop(puck , attribute, intelligence).
+prop(pugna , attribute, intelligence).
+prop(queen_of_pain , attribute, intelligence).
+prop(rubick , attribute, intelligence).
+prop(shadow_demon , attribute, intelligence).
+prop(shadow_shaman , attribute, intelligence).
+prop(silencer , attribute, intelligence).
+prop(skywrath_mage , attribute, intelligence).
+prop(storm_spirit , attribute, intelligence).
+prop(techies , attribute, intelligence).
+prop(tinker , attribute, intelligence).
+prop(visage , attribute, intelligence).
+prop(void_spirit , attribute, intelligence).
+prop(warlock , attribute, intelligence).
+prop(windranger , attribute, intelligence).
+prop(winter_wyvern , attribute, intelligence).
+prop(witch_doctor , attribute, intelligence).
+prop(zeus , attribute, intelligence).
